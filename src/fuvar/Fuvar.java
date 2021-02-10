b1345c68c9ef4de5960858ea1c9f63dfa6faf86e
tothbalazs <tbalazsmark@gmail.com>
2021-02-10 11:47:18 +0100

    2. feladat

--- src/fuvar/Fuvar.java
+++ src/fuvar/Fuvar.java
@@ -1,9 +1,15 @@
 package fuvar;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Fuvar {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        List<String> sorok = Files.readAllLines(Paths.get("fuvar.csv"));
+                
     }
     
 }
